0fcc4268ae65eac22f6562d9a0708d31eeda54b4
Peter Upfold <pgithub@upfold.org.uk>
2019-01-23 20:50:25 +0000

    Add first test

---
+++ simple-temp-queue-pusher.py
@@ -0,0 +1,27 @@
+# simple-temp-queue-pusher
+# Push a simple temperature sensor reading into an Azure Service Bus Queue
+#
+# Copyright 2019 Peter Upfold
+#
+# Licensed under the Apache 2.0 Licence.
+
+
+from azure.servicebus import ServiceBusClient, Message, Queue
+import yaml
+import subprocess
+
+config_file = open('config.yml', 'r')
+config = yaml.safe_load(config_file)
+
+bus_service = ServiceBusService(
+    service_namespace=config.namespace,
+    shared_access_key_name=config.shared_access_key_name,
+    shared_access_key_value=config.shared_access_key_value)
+
+queue_client = bus_service.get_queue(config.queue_name)
+
+
+proc = subprocess.Popen(['./simple-temp-readout', '/dev/hidraw1', '0x01', '0x80', '0x33', '0x01', '0x00', '0x00', '0x00', '0x00'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, err = proc.communicate()
+
+queue_client.send(output)
