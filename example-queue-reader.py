61eafd108309af1b1a74c805e6877d299f0ad5ea
Peter Upfold <pgithub@upfold.org.uk>
2019-01-23 21:07:42 +0000

    Example ServiceBus Queue reader

---
+++ example-queue-reader.py
@@ -0,0 +1,25 @@
+# example-queue-reader
+# Simple script to test receiving events through the queue
+#
+# Copyright 2019 Peter Upfold
+#
+# Licensed under the Apache 2.0 Licence.
+
+
+from azure.servicebus import ServiceBusClient
+import yaml
+
+config_file = open('config.yml', 'r')
+config = yaml.safe_load(config_file)
+
+bus_service = ServiceBusClient(
+    service_namespace=config['namespace'],
+    shared_access_key_name=config['shared_access_key_name'],
+    shared_access_key_value=config['shared_access_key_value'])
+
+queue_client = bus_service.get_queue(config['queue_name'])
+
+messages = queue_client.get_receiver()
+for message in messages:
+    print(message)
+    message.complete()
