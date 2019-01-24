c52b799cea521e2575c918965cd111f63d665db0
Peter Upfold <pgithub@upfold.org.uk>
2019-01-24 19:43:08 +0000

    Ignore zero length messages

--- example-queue-reader.py
+++ example-queue-reader.py
@@ -21,7 +21,10 @@ queue_client = bus_service.get_queue(config['queue_name'])
 
 messages = queue_client.get_receiver()
 for message in messages:
-    print(message)
-    with open(config['output_file'], 'w') as output_file:
-        output_file.write(str(message))
+    if len(message) > 0:
+	    print(message)
+	    with open(config['output_file'], 'w') as output_file:
+		output_file.write(str(message))
+    else:
+            print('Zero-length message')
     message.complete()
